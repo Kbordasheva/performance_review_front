71a898bec33d4bb07e1e173c4e859a5c979620d4
Kate Bardashava <k.bardashava@itechart-group.com>
2021-08-03 22:10:06 +0300

    Add +/- for PR

--- src/components/Profile/PerformanceReview/ReviewDetailsItem/ReviewDetailsItem.js
+++ src/components/Profile/PerformanceReview/ReviewDetailsItem/ReviewDetailsItem.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Goal from "./Goal/Goal";
 import "./ReviewDetailsItem.scss"
+import CheckboxHelp from "../../../Shared/CheckboxHelp/CheckBoxHelp";
 
 const ReviewDetailsItem = (props) => {
+  const [isFormVisible, setIsFormVisible] = useState(false)
+
+  const toggle = () => {
+        setIsFormVisible(!isFormVisible)
+    }
 
   const { review, review_id } = props;
 
   return (
     <div className="review-year">
-      <h4>{review.year}</h4>
-                 <div key="all-goals">
-                    <Goal
-                        reviewId={review_id}
-                        goalsInfo={review.goals}
-                    />
-                </div>
+      <CheckboxHelp setIsFormVisible={toggle} title={review.year}/>
+      {isFormVisible &&
+      <div key="all-goals">
+        <Goal
+          reviewId={review_id}
+          goalsInfo={review.goals}
+        />
+      </div>
+      }
     </div>
   );
 };
